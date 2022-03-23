b747f97d733f86948056a1f8ebd5877d87b64542
Nairod <do.gaspart@gmail.com>
2022-03-23 11:11:11 +0100

    Ajout de la methode pour avoir le CSS dans le PremiersElementsGraphiques

--- src/main/java/premiereVue/PremiersElementsGraphiques.java
+++ src/main/java/premiereVue/PremiersElementsGraphiques.java
@@ -6,20 +6,25 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.scene.control.Label;
 
+import java.io.File;
+
 public class PremiersElementsGraphiques extends Application {
-    public void start(Stage stage)   {
+    public void start(Stage stage) {
         VBox root = new VBox(10);
         Label labelHello = new Label("Hello");
         root.getChildren().add(labelHello);
         Label labelHelloBis = new Label("Hello Java FX");
         root.getChildren().add(labelHelloBis);
-        Scene scene = new Scene(root,600,90);
+
+        Scene scene = new Scene(root, 600, 90);
         stage.setScene(scene);
         stage.setTitle("NOproblem");
         stage.show();
+        File fileCss = new File("src/main/css" + File.separator + "premiersStyles.css");
+        scene.getStylesheets().add(fileCss.toURI().toString());
     }
+
     public static void main(String[] args) {
         Application.launch();
     }
-
 }
